e3339268d59224e0411bf670841d15a71b2b517e
marceloferreiracampos07 <junior87721727@gmail.com>
2026-01-11 16:07:19 -0300

    user controller

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -1,18 +1,4 @@
-const User = require('../models/user.js');
-const bcrypt = require('bcryptjs');
-const path = require('path');
-
-class UserController {
-    
-    static showlogin(req, res) {
-        res.sendFile(path.join(__dirname, '../views/login.html'));
-    }
-
-    static showcadastro(req, res) {
-        res.sendFile(path.join(__dirname, '../views/cadastro.html'));
-    }
-
-    static async salvarlogin(req, res) {
+static async salvarlogin(req, res) {
     const { name, email, password, telefone } = req.body;
 
     try {
@@ -33,48 +19,10 @@ class UserController {
 
         await User.create(userData);
         
-        
-        
         res.redirect('/login');
 
     } catch (error) {
-        console.error('Erro no cadastro:', error);
+        console.error('Erro detalhado no Sequelize:', error);
         res.status(500).send("Erro ao criar conta.");
     }
 }
-    static async verificarlogin(req, res) {
-        const { email, password } = req.body;
-
-        try {
-            const usuarioencontrado = await User.findOne({ where: { email: email } });
-
-            if (!usuarioencontrado) {
-                return res.status(400).send('Usuário não encontrado.');
-            }
-
-            const senhaBate = bcrypt.compareSync(password, usuarioencontrado.password);
-
-            if (!senhaBate) {
-                return res.status(400).send('Senha incorreta.');
-            }
-
-            req.session.userid = usuarioencontrado.id;
-
-            req.session.save(() => {
-                res.redirect('/tasks/add'); 
-            });
-
-        } catch (error) {
-            console.error('Erro no login:', error);
-            res.status(500).send('Erro interno no servidor.');
-        }
-    }
-
-    static sair(req, res) {
-        req.session.destroy(() => {
-            res.redirect('/login');
-        });
-    }
-}
-
-module.exports = UserController;

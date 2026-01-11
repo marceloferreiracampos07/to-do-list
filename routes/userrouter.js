6328c1a83f162ad401483051c0eaec452aa5bd6e
marceloferreiracampos07 <junior87721727@gmail.com>
2026-01-11 01:58:29 -0300

    Ajuste definitivo de case sensitive para o helper

--- routes/userrouter.js
+++ routes/userrouter.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/usercontroller.js');
-const { checkAuth } = require('./helpers/auth.js'); // Sem letras maiúsculas se a pasta for 'helpers'
+const { checkAuth } = require('..helpers/auth.js'); 
 
 router.get('/login', checkAuth ,UserController.showlogin);
 router.post('/login', checkAuth,UserController.verificarlogin);

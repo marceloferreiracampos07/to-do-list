e4e9739028b8b27ace484e75c687c99288f8a08d
marceloferreiracampos07 <junior87721727@gmail.com>
2026-01-11 02:11:17 -0300

    Ajuste nas rotas

--- routes/taskrouter.js
+++ routes/taskrouter.js
@@ -1,25 +1,28 @@
-const express = require('express')
-const router = express.Router()
-const task_controller = require("../controllers/taskcontroller.js") 
+const express = require('express');
+const router = express.Router();
+const task_controller = require("../controllers/taskcontroller.js");
+const { checkAuth } = require('../helpers/auth.js'); // Importando o protetor
 
-router.get('/add', task_controller.createtask) 
+router.get('/add', checkAuth, task_controller.createtask); 
 
 
-router.post('/add', task_controller.createtasksave)
+router.post('/add', checkAuth, task_controller.createtasksave);
 
-router.get('/', task_controller.showTasks)
 
-router.get('/all-data', task_controller.showAllData);
+router.get('/', checkAuth, task_controller.showTasks);
 
-router.post('/remove',task_controller.excluirtarefa)
+router.get('/all-data', checkAuth, task_controller.showAllData);
 
-router.get('/edit/:id',task_controller.atualizartarefa)
 
-router.post('/update', task_controller.atualizartarefaupdate);
+router.post('/remove', checkAuth, task_controller.excluirtarefa);
 
-router.post('/concluir',task_controller.tarefaconcluida)
 
-router.get('/concluidas-dados',task_controller.tarefaconlcuidaalldata)
+router.get('/edit/:id', checkAuth, task_controller.atualizartarefa);
+router.post('/update', checkAuth, task_controller.atualizartarefaupdate);
 
-router.get('/concluidas-page',task_controller.mostrarPaginaConcluidas)
-module.exports = router
+
+router.post('/concluir', checkAuth, task_controller.tarefaconcluida);
+router.get('/concluidas-dados', checkAuth, task_controller.tarefaconlcuidaalldata);
+router.get('/concluidas-page', checkAuth, task_controller.mostrarPaginaConcluidas);
+
+module.exports = router;

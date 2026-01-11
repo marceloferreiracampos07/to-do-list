2729d58889b53bf68bc697224ad2bc6238eedb3f
marceloferreiracampos07 <junior87721727@gmail.com>
2026-01-11 02:05:11 -0300

    Ajuste no  helper

--- routes/userrouter.js
+++ routes/userrouter.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const UserController = require('../controllers/usercontroller.js');
-const { checkAuth } = require('..helpers/auth.js'); 
+const { checkAuth } = require('../helpers/auth.js'); 
 
-router.get('/login', checkAuth ,UserController.showlogin);
-router.post('/login', checkAuth,UserController.verificarlogin);
+router.get('/login', UserController.showlogin);
+router.post('/login', UserController.verificarlogin);
 
-router.get('/cadastro',checkAuth, UserController.showcadastro);
-router.post('/cadastro', checkAuth,UserController.salvarlogin);
+router.get('/cadastro', UserController.showcadastro);
+router.post('/cadastro', UserController.salvarlogin);
 
-router.get('/logout', checkAuth,UserController.sair);
+
+router.get('/logout', checkAuth, UserController.sair);
 
 module.exports = router;

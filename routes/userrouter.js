35ae475511c939c026d83e736ff7db876e502a81
marceloferreiracampos07 <junior87721727@gmail.com>
2026-01-11 01:49:51 -0300

    consertando erros de rota

--- routes/userrouter.js
+++ routes/userrouter.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const UserController = require('../controllers/usercontroller.js');
 const { checkAuth } = require('./helpers/auth'); // Sem letras maiúsculas se a pasta for 'helpers'
 
-router.get('/login', check ,UserController.showlogin);
-router.post('/login', check,UserController.verificarlogin);
+router.get('/login', checkAuth ,UserController.showlogin);
+router.post('/login', checkAuth,UserController.verificarlogin);
 
-router.get('/cadastro',check, UserController.showcadastro);
-router.post('/cadastro', check,UserController.salvarlogin);
+router.get('/cadastro',checkAuth, UserController.showcadastro);
+router.post('/cadastro', checkAuth,UserController.salvarlogin);
 
-router.get('/logout', check,UserController.sair);
+router.get('/logout', checkAuth,UserController.sair);
 
 module.exports = router;
